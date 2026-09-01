cde81c476d0bf685e280486d191a60c100ceecc3
Marco Rendon <rendon@marconet.local>
2026-09-01 11:16:47 -0600

    Fix: conflicto de position CSS en ChatWidget

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -7,7 +7,7 @@ const ChatWidget: React.FC = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 w-[60px] h-[60px] relative">
+    <div className="fixed bottom-6 right-6 z-50 w-[60px] h-[60px]">
       {/* Botón de cerrar */}
       <button
         onClick={() => setIsVisible(false)}
